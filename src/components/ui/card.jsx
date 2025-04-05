f650b6c8da2ba849ac707e58ef09b2fc0400f0b2
AJREarlyReleases <ajrearlyreleases1@gmail.com>
2025-04-05 13:54:45 -0400

    Small changes.

--- src/components/ui/card.jsx
+++
@@ -1,7 +0,0 @@
-export function Card({ className = "", children }) {
-    return <div className={`rounded-2xl bg-[#151515] shadow-xl overflow-hidden ${className}`}>{children}</div>;
-  }
-  
-  export function CardContent({ className = "", children }) {
-    return <div className={`p-4 ${className}`}>{children}</div>;
-  }  
